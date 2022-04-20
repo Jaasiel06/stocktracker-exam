a468eff5a66ed08b0ffacdf0a414be76b8b825fc
Jaasiel Diaz <jasiel_diaz@hotmail.com>
2022-04-20 00:50:44 -0400

    ini reactive form

--- src/app/modules/stocks/stocks-track/stocks-track.component.ts
+++ src/app/modules/stocks/stocks-track/stocks-track.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-stocks-track',
   templateUrl: './stocks-track.component.html',
   styleUrls: ['./stocks-track.component.css']
 })
+
+
+
 export class StocksTrackComponent implements OnInit {
 
   constructor() { }
 
+  stockTrackForm = new FormGroup({
+    stockSymbol: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(15)
+    ])
+  });
+
   ngOnInit(): void {
   }
 
+  get stockSymbol() { return this.stockTrackForm.get('stockSymbol'); }
+  trackStock() {
+    alert(1)
+  }
 }
